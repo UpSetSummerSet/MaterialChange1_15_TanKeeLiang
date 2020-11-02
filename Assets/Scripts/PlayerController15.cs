5b2b9c637afd3438882e9e60999e646dfb05b031
UpSetSummerSet <72717739+UpSetSummerSet@users.noreply.github.com>
2020-11-02 13:06:45 +0800

    MaterialChange1_16_TanKeeLiang

--- Assets/Scripts/PlayerController15.cs
+++ Assets/Scripts/PlayerController15.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 
 public class PlayerController15 : MonoBehaviour
 {
-    bool isOnGround = true;
+    int isOnGround = 0;
 
     float jumpForce = 10.0f;
     float gravityModifier = 2.0f;
+    float zLimit = 4.0f;
+    float xLimit = 4.0f;
+    float speed = 10.0f;
 
     Rigidbody playerRb;
+    Renderer playerRdr;
+
+    public Material[] playerMtrs;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,27 +30,55 @@ public class PlayerController15 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        PlayerJump;
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        //Move Player (GameObject) according to user interactions
+        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+
+        if (transform.position.z < -zLimit)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zLimit);
+            playerRdr.material.color = playerMtrs[2].color;
+        }
+        else if (transform.position.z > zLimit)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zLimit);
+            playerRdr.material.color = playerMtrs[3].color;
+        }
+        if (transform.position.x < -xLimit)
+        {
+            transform.position = new Vector3(-xLimit, transform.position.y, transform.position.z);
+            playerRdr.material.color = playerMtrs[4].color;
+        }
+        else if (transform.position.x > xLimit)
+        {
+            transform.position = new Vector3(xLimit, transform.position.y, transform.position.z);
+            playerRdr.material.color = playerMtrs[5].color;
+        }
+        PlayerJump();
     }
 
-    private void OnCllisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("GamePlane"))
         {
-            isOnGround = true;
+            isOnGround = 0;
 
-            playerRdr.material.color = playerMtrs[4].color;
+            playerRdr.material.color = playerMtrs[0].color;
         }
     }
 
     private void PlayerJump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround < 1)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isOnGround = false;
 
-            playerRdr.material.color = playerMtrs[2].color;
+            isOnGround++;
+
+            playerRdr.material.color = playerMtrs[1].color;
         }
     }
 }
